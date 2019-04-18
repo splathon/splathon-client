43866d2b80ba5d3f96017dd664e293fd510448c9
koutalou <k.koutalou@gmail.com>
2019-04-19 04:42:45 +0900

    :wrench: bind each team result view to API I/F

--- lib/views/myresult.dart
+++ lib/views/myresult.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:splathon_app/styles/text.dart';
 import 'package:splathon_app/styles/color.dart';
 import 'package:english_words/english_words.dart';
+import 'package:splathon_app/views/resultdetail.dart';
+import 'dart:async';
+import 'package:openapi/api.dart';
 
 class EachResult extends StatefulWidget {
   EachResult({Key key}) : super(key: key);
@@ -11,17 +14,59 @@ class EachResult extends StatefulWidget {
 }
 
 class _EachResultState extends State<EachResult> {
+  Teams _model;
+  Results _results;
+  String dropdownValue = 'NuRItaclesカスタム'; // TODO: 暫定固定実装、ログイン処理実装して自身のTeamNameが取得できるようになったらここに入れる
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTeams();
+    fetchResult(84); // TODO: 暫定固定実装、ログイン処理実装して自身のTeamIDが取得できるようになったらここに入れる
+  }
+
+  Future fetchTeams() async {
+    var client = new DefaultApi();
+    var result = client.listTeams(9);
+    result.then(
+      (resultsObj) => setState(() { this._model = resultsObj; } )
+    );
+  }
+
+  Future fetchResult(int teamId) async {
+    var client = new ResultApi();
+    var result = client.getResult(9, teamId: teamId);
+    result.then(
+      (resultsObj) => setState(() { this._results = resultsObj; } )
+    );
+  }
+
+  void changedTeam(String teamName) {
+    _model.teams.forEach((team) => 
+      team.name == teamName ? fetchResult(team.id) : empty(),
+    );
+  }
+  void empty() {}
+
   @override
   Widget build(BuildContext context) {
     return buildEachResult();
   }
 
   Widget buildEachResult() {
-    String dropdownValue = 'AplatoonZZZからあげ定食';
+    if (_model == null || _results == null) {
+      // Loading
+      return new Center(
+        child: const CircularProgressIndicator(),
+      );
+    }
+    
     double screenWidth = MediaQuery.of(context).size.width;
+    
+    List<String> teamNames = _model.teams.map((team) => team.name).toList();
 
     return new ListView.builder(
-      itemCount: 20,
+      itemCount: _results.qualifiers.length + 1,
       itemBuilder: (BuildContext context, i) {
         if (i == 0) {
           return Container(
@@ -45,9 +90,9 @@ class _EachResultState extends State<EachResult> {
                     setState(() {
                       dropdownValue = newValue;
                     });
+                    changedTeam(newValue);
                   },
-                  items: <String>['AplatoonZZZからあげ定食', 'BplatoonXXXカレーライス', 'CplatoonYYYハンバーグ', 'DplatoonWWWステーキ']
-                      .map<DropdownMenuItem<String>>((String value) {
+                  items: teamNames.map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value, style: topNameStyle),
@@ -58,87 +103,129 @@ class _EachResultState extends State<EachResult> {
             ),
           );
         }
-        return new Container(
-          foregroundDecoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            border: Border.all(
-              color: borderblueColor,
-              width: 1,
-            )
-          ),
-          color: Colors.white,
-          margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
-          padding: const EdgeInsets.only(left: 14.0, right: 14.0),
-          height: 82.0,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 14.0,
-                      child: Text('予選 ROUND1 B卓 第4試合', style: resultTitleStyle),
-                      ),
-                    Container(
-                      height: 36.0,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          SizedBox(
-                            width: screenWidth * 0.35,
-                            child: Text('AplatoonZZZからあげ定食', style: resultNameStyle, maxLines: 1,),
-                          ),
-                          Text('vs', style: resultNameStyle),
-                          SizedBox(
-                            width: screenWidth * 0.35,
-                            child: Text('BplatoonXXXカレーライス', style: resultNameStyle, maxLines: 1,),
-                          ),                    
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 18.0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            margin: const EdgeInsets.only(left: 6),
-                            padding: const EdgeInsets.only(left: 10.0, right: 10.0,),
-                            height: 17.0,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              color: winColor,
+
+        // SPEC: room, matchはRoundごとに各Team1つずつになる前提
+        Round round = _results.qualifiers[i - 1];
+        Room room = round.rooms.first;
+        Match2 match = room.matches.first;
+        int order = match.order;
+
+        return GestureDetector(
+          onTap: () { 
+            Navigator.push(context, new MaterialPageRoute<Null>(
+              settings: const RouteSettings(name: "/result"),
+              builder: (BuildContext context) => new ResultDetail(match),
+            ));
+          },
+          child: new Container(
+            foregroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: borderblueColor,
+                width: 1,
+              )
+            ),
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+            padding: const EdgeInsets.only(left: 14.0, right: 14.0),
+            height: 82.0,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 14.0,
+                        child: Text(round.name + " " + room.name + " 第$order試合", style: resultTitleStyle),
+                        ),
+                      Container(
+                        height: 36.0,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            SizedBox(
+                              width: screenWidth * 0.35,
+                              child: Text(match.teamAlpha.name, style: resultNameStyle, maxLines: 1,),
                             ),
-                            child: Text('WIN', style: resultResultStyle),
-                          ),
-                          SizedBox(
-                            width: 20.0,
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(left: 6),
-                            padding: const EdgeInsets.only(left: 10.0, right: 10.0,),
-                            height: 20.0,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              color: loseColor,
-                            ),
-                            child: Text('LOSE', style: resultResultStyle),
-                          )
-                        ]
+                            Text('vs', style: resultNameStyle),
+                            SizedBox(
+                              width: screenWidth * 0.35,
+                              child: Text(match.teamBravo.name, style: resultNameStyle, maxLines: 1,),
+                            ),                    
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      winloseView(match.winner),
+                    ],
+                  ),
                 ),
-              ),
-              Image.asset('assets/images/arrowRight.png')
-            ],
+                Image.asset('assets/images/arrowRight.png')
+              ],
+            ),
           ),
         );
       }
+    );
+  }
+
+    Widget winloseView(String winner) {
+    if (winner != 'alpha' && winner != 'bravo') {
+      return Container(
+        height: 18.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(left: 6),
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0,),
+              height: 17.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: drawColor,
+              ),
+              child: Text('DRAW', style: resultResultStyle),
+            ),
+          ]
+        ),
+      );
+    }
+
+    bool isWinAlpha = winner == 'alpha';
+
+    return Container(
+      height: 18.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(left: 6),
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0,),
+            height: 17.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: isWinAlpha ? winColor : loseColor,
+            ),
+            child: Text(isWinAlpha ? 'WIN' : 'LOSE', style: resultResultStyle),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 6),
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0,),
+            height: 20.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: isWinAlpha ? loseColor : winColor,
+            ),
+            child: Text(isWinAlpha ? 'LOSE' : 'WIN', style: resultResultStyle),
+          )
+        ]
+      ),
     );
   }
 
