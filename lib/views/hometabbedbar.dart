721532b75b3cdff61e5cb1077a56cc8dcccd23e5
koutalou <k.koutalou@gmail.com>
2019-04-21 03:25:39 +0900

    :sparkles: Support Firebase Cloud Messaging, display notification popup view when app foregournd

--- lib/views/hometabbedbar.dart
+++ lib/views/hometabbedbar.dart
@@ -5,6 +5,7 @@ import 'package:splathon_app/views/result.dart';
 import 'package:splathon_app/views/notification.dart';
 import 'package:splathon_app/views/reception.dart';
 import 'package:splathon_app/views/rankings.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 /**
  * Reference
@@ -18,12 +19,14 @@ class HomeTabbedBar extends StatefulWidget {
 // SingleTickerProviderStateMixin is used for animation
 class HomeTabbedBarState extends State<HomeTabbedBar> with SingleTickerProviderStateMixin {
   TabController controller;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   @override
   void initState() {
     super.initState();
 
     controller = new TabController(length: 4, vsync: this);
+    setupFirebaseCloudMessaging(context);
   }
 
   @override
@@ -79,4 +82,82 @@ class HomeTabbedBarState extends State<HomeTabbedBar> with SingleTickerProviderS
       ),
     );
   }
+  
+  // TODO: 隠蔽化したい
+  void setupFirebaseCloudMessaging(BuildContext context) {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('onMessage: $message');
+        buildDialog(context, message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onLaunch: $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(sound: true, badge: true, alert: true)
+    );
+    _firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
+      print('Settings registered: $settings');
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print('Push Teken: $token');
+    });
+    _firebaseMessaging.subscribeToTopic('/topic/all');
+  }
+
+  buildDialog(BuildContext context, Map<String, dynamic> message) {
+    String text = '${message['aps']['alert']['body']}'; // iOS APNs
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext build) {
+        return new AlertDialog(
+          titlePadding: EdgeInsets.all(0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))
+          ),
+          title: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              color: splaBlueColor,
+            ),
+            padding: const EdgeInsets.all(10),
+            child: Center(child: Text('お知らせ', style: popupTitleStyle,),),
+          ),
+          content: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: 100),
+            child: Text('$text', style: popupMessageStyle,),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('CLOSE'),
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static const TextStyle popupTitleStyle = TextStyle(
+    fontFamily: 'Splatfont',
+    color: Colors.white,
+    fontSize: 20.0,
+  );
+
+  static const TextStyle popupMessageStyle = TextStyle(
+    fontFamily: 'Splatfont',
+    color: blackColor,
+    fontSize: 16.0,
+  );
 }
